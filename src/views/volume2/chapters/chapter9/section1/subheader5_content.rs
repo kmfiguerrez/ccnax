4a3dae01dbd61b477a79501e096a56d3be286256
kmfiguerrez <kmfiguerrez@gmail.com>
2026-09-02 02:37:58 +0000

    Add red note in v2p3c9s1sh5

--- src/views/volume2/chapters/chapter9/section1/subheader5_content.rs
+++ src/views/volume2/chapters/chapter9/section1/subheader5_content.rs
@@ -1,170 +1,183 @@
 use dioxus::prelude::*;
 
-use crate::utils::text_command::text_command;
+use crate::{components::red_div::RedNote, utils::text_command::text_command};
 
 #[component]
 pub fn Content() -> Element {
     rsx! {
-        p { class: "mb-3",
-            "With the information in Table 9-2 ( "
-            strong { "Table 9-2 can be viewed in the previous content" }
-            " ), configuring syslog in a Cisco IOS 
+        div { class: "pb-4",
+            p { class: "mb-3",
+                "With the information in Table 9-2 ( "
+                strong { "Table 9-2 can be viewed in the previous content" }
+                " ), configuring syslog in a Cisco IOS 
             router or switch should be relatively straightforward."
-            br {}
-            "Example 9-2 shows a sample, based on Figure 9-4."
-            br {}
-            "The figure shows a syslog server at IP address 172.16.3.9."
-            br {}
-            "Both switches and both routers will use the same configuration shown in Example 9-2, 
+                br {}
+                "Example 9-2 shows a sample, based on Figure 9-4."
+                br {}
+                "The figure shows a syslog server at IP address 172.16.3.9."
+                br {}
+                "Both switches and both routers will use the same configuration shown in Example 9-2, 
             although the example shows the configuration process on a single device, router R1."
-        }
+            }
 
-        img {
-            class: "mb-3",
-            src: asset!("/assets/static/v2p3c9s1sh3f9-4.png", AssetOptions::image().with_avif()),
-            alt: "Figurez 9-4: Sample Network Used in Logging Examples",
-        
-        }
-        img {
-            class: "mb-4",
-            src: asset!("/assets/static/v2p3c9s1sh3ex9-2.png", AssetOptions::image().with_avif()),
-            alt: "Example 9-2: Syslog configuration on Router 1",
-        
-        }
-        p {
-            strong { class: "text-red-500", "Note in example 9-2:" }
-            " On Cisco IOS version 03.16.05.S in Packet Tracer"
-        }
-        ul { class: "list-disc pl-4 mb-4",
-            li {
-                "You can't specify the the severity level using both the name and number in the commands for "
-                {text_command("logging console")}
-                ", "
-                {text_command("logging monitor")}
-                " and "
-                {text_command("logging buffered")}
-                " as they default to receive messages of all levels."
+            img {
+                class: "mb-3",
+                src: asset!("/assets/static/v2p3c9s1sh3f9-4.png", AssetOptions::image().with_avif()),
+                alt: "Figurez 9-4: Sample Network Used in Logging Examples",
             
             }
-            li {
-                "For the command: "
-                {text_command("logging buffered")}
-                ", you have to specify the amount of memory (buffer size)."
+            img {
+                class: "mb-4",
+                src: asset!("/assets/static/v2p3c9s1sh3ex9-2.png", AssetOptions::image().with_avif()),
+                alt: "Example 9-2: Syslog configuration on Router 1",
+            
             }
-            li {
-                "For the command: "
-                {text_command("logging trap")}
-                ", you can only set the debug level option which means will also receive messages of all levels."
-            }
-        }
 
-        p { class: "mb-4",
-            "First, note that the example configures the same message level at the console and for terminal 
+            RedNote {
+                p {
+                    strong { "Note in example 9-2:" }
+                    " On Cisco IOS version 03.16.05.S in Packet Tracer"
+                }
+                ul { class: "list-disc pl-4 mb-4",
+                    li {
+                        "You can't specify the the severity level using both the name and number in the commands for "
+                        span { class: "font-bold", "logging console" }
+                        ", "
+                        span { class: "font-bold", "logging monitor" }
+                        " and "
+                        span { class: "font-bold", "logging buffered" }
+                        " as they default to receive messages of all levels."
+                    
+                    }
+                    li {
+                        "For the command: "
+                        span { class: "font-bold", "logging buffered" }
+                        ", you have to specify the amount of memory (buffer size)."
+                    }
+                    li {
+                        "For the command: "
+                        span { class: "font-bold", "logging trap" }
+                        ", you can only set the debug level option which means will also receive messages of all levels."
+                    }
+                }
+            }
+
+            p { class: "mb-4",
+                "First, note that the example configures the same message level at the console and for terminal 
             monitoring (level 7, or debug), and the same level for both buffered and logging to the
             syslog server (level 4, or warning)."
-            br {}
-            "The levels may be set using the numeric severity level or the name as shown earlier in Figure 9-3."
-        }
+                br {}
+                "The levels may be set using the numeric severity level or the name as shown earlier in Figure 9-3."
+            }
 
-        p { class: "mb-3",
-            "The "
-            {text_command("show logging")}
-            " command confirms those same configuration settings and also lists the
+            p { class: "mb-3",
+                "The "
+                {text_command("show logging")}
+                " command confirms those same configuration settings and also lists the
             log messages per the logging buffered configuration."
-            br {}
-            "Example 9-3 shows a sample, with the
+                br {}
+                "Example 9-3 shows a sample, with the
             configuration settings to match Example 9-2 highlighted in gray."
-        }
-        figure { class: "mb-4",
-            figcaption {
-                span { class: "font-bold", "Example 9-3: " }
-                "Viewing the Configured Log Settings per the Earlier Example"
             }
-            img {
-                class: "mb-4",
-                src: asset!("/assets/static/v2p3c9s1sh3ex9-3a.png", AssetOptions::image().with_avif()),
-                alt: "Example 9-3: Ouput of the show logging command",
+            figure { class: "mb-4",
+                figcaption {
+                    span { class: "font-bold", "Example 9-3: " }
+                    "Viewing the Configured Log Settings per the Earlier Example"
+                }
+                img {
+                    class: "mb-4",
+                    src: asset!("/assets/static/v2p3c9s1sh3ex9-3a.png", AssetOptions::image().with_avif()),
+                    alt: "Example 9-3: Ouput of the show logging command",
+                }
             }
-        }
 
-        p { class: "mb-4",
-            strong {
-                "You might notice by now that knowing the names of all eight log message levels can be handy if you want to understand 
+            p { class: "mb-4",
+                strong {
+                    "You might notice by now that knowing the names of all eight log message levels can be handy if you want to understand 
                 the output of the commands."
-            }
-            br {}
-            " Most of the show commands list the log message levels by name, not by number."
-            br {}
-            " As you can see in the green highlights in this example, two levels list “debug,” and two list “warning,” even though 
+                }
+                br {}
+                " Most of the show commands list the log message levels by name, not by number."
+                br {}
+                " As you can see in the green highlights in this example, two levels list “debug,” and two list “warning,” even though 
             some of the configuration commands referred to those levels by number."
-        }
-
-        p { class: "mb-4",
-            "Also, you cannot know this from the output, but in Example 9-3, router R1 has no buffered log messages."
-            br {}
-            " (Note the counter value of 0 for buffered logging messages.)"
-            br {}
-            " If any log messages had been buffered, the actual log messages would be listed at the end of the command."
-            br {}
-            "In this case, the router just booted, and no messages has been buffered yet."
-            br {}
-            "(You could also clear out the old messages from the log with the "
-            {text_command("clear logging")}
-            " EXEC command.)"
-            br {}
-            strong { class: "text-red-500", "Note: " }
-            "On Cisco IOS version 03.16.05.S in Packet Tracer, the "
-            {text_command("clear logging")}
-            " is not available!"
-        }
-
-        p { class: "mb-4",
-            "The next example shows the difference between the current severity levels."
-            br {}
-            "This example shows the user disabling interface G0/1 on R1 with the "
-            {text_command("shutdown")}
-            " command and then reenabling it with the "
-            {text_command("no shutdown")}
-            " command."
-            br {}
-            "If you look closely at the highlighted messages, you will see several severity 5 messages and one severity 3 message."
-            br {}
-            "The "
-            {text_command("logging buffered 4")}
-            " global configuration command on R1 (see Example 9-2) means that R1 will not buffer the
-            severity level 5 log messages, but it will buffer the severity level 3 message and more severe messages."
-            br {}
-            "Example 9-4 ends by showing that log message at the end of the output of the "
-            {text_command("show logging")}
-            " command."
-        }
-
-        figure { class: "mb-4",
-            figcaption {
-                span { class: "font-bold", "Example 9-4: " }
-                "Seeing Severity 3 and 5 Messages at the Console, and Severity 3 Only in the Buffer"
             }
-            img {
-                class: "mb-4",
-                src: asset!("/assets/static/v2p3c9s1sh3ex9-4.png", AssetOptions::image().with_avif()),
-                alt: "Example 9-4: Ouput of the configuration and show logging commands",
-            }
-            strong { class: "text-red-500", "Note in Example 9-4: " }
-            " On Cisco IOS version 03.16.05.S in Packet Tracer, logging by buffer doesn't work!"
-        }
 
-        h3 { class: "font-semibold mb-1", "REMEMBER" }
-        ul { class: "list-disc pl-4",
-            li {
-                "It's best to know all the eight log messages to understand the output of the commands."
-            }
-            li { "Most of the show commands list the log message levels by name, not by number." }
-            li {
-                "You could also clear out the old messages from the log with the "
+            p { class: "mb-4",
+                "Also, you cannot know this from the output, but in Example 9-3, router R1 has no buffered log messages."
+                br {}
+                " (Note the counter value of 0 for buffered logging messages.)"
+                br {}
+                " If any log messages had been buffered, the actual log messages would be listed at the end of the command."
+                br {}
+                "In this case, the router just booted, and no messages has been buffered yet."
+                br {}
+                "(You could also clear out the old messages from the log with the "
                 {text_command("clear logging")}
-                " EXEC command."
+                " EXEC command.)"
+            }
+
+            RedNote {
+                p {
+                    strong { "Note:" }
+                    " On Cisco IOS version 03.16.05.S in Packet Tracer, the "
+                    span { class: "font-bold", "clear logging" }
+                    " is not available!"
+                }
+            }
+
+            p { class: "mb-4",
+                "The next example shows the difference between the current severity levels."
+                br {}
+                "This example shows the user disabling interface G0/1 on R1 with the "
+                {text_command("shutdown")}
+                " command and then reenabling it with the "
+                {text_command("no shutdown")}
+                " command."
+                br {}
+                "If you look closely at the highlighted messages, you will see several severity 5 messages and one severity 3 message."
+                br {}
+                "The "
+                {text_command("logging buffered 4")}
+                " global configuration command on R1 (see Example 9-2) means that R1 will not buffer the
+            severity level 5 log messages, but it will buffer the severity level 3 message and more severe messages."
+                br {}
+                "Example 9-4 ends by showing that log message at the end of the output of the "
+                {text_command("show logging")}
+                " command."
+            }
+
+            figure { class: "mb-4",
+                figcaption {
+                    span { class: "font-bold", "Example 9-4: " }
+                    "Seeing Severity 3 and 5 Messages at the Console, and Severity 3 Only in the Buffer"
+                }
+                img {
+                    class: "mb-4",
+                    src: asset!("/assets/static/v2p3c9s1sh3ex9-4.png", AssetOptions::image().with_avif()),
+                    alt: "Example 9-4: Ouput of the configuration and show logging commands",
+                }
+            }
+
+            RedNote {
+                p {
+                    strong { "Note in Example 9-4:" }
+                    " On Cisco IOS version 03.16.05.S in Packet Tracer, logging by buffer doesn't work!"
+                }
+            }
+
+            h3 { class: "font-semibold mb-1", "REMEMBER" }
+            ul { class: "list-disc pl-4",
+                li {
+                    "It's best to know all the eight log messages to understand the output of the commands."
+                }
+                li { "Most of the show commands list the log message levels by name, not by number." }
+                li {
+                    "You could also clear out the old messages from the log with the "
+                    {text_command("clear logging")}
+                    " EXEC command."
+                }
             }
         }
-
     }
 }
