b08f26c0492edd27b1411ef9b9e13321216f6cfb
kmfiguerrez <kmfiguerrez@gmail.com>
2026-09-11 04:10:04 +0000

    Make v1p3c8s1sh1 content.

--- src/components/subheader_content.rs
+++ src/components/subheader_content.rs
@@ -71,6 +71,10 @@ pub fn SubheaderContent(volume_id: u32, part_id: u32, chapter_id: u32, section_i
                 (1, 2, 7, 2, 3) => rsx! {
                     volume1::chapter7::section2::subheader3_content::Content {}
                 },
+                // Start of volume 1, part 3, chapter 8, section 1
+                (1, 3, 8, 1, 1) => rsx! {
+                    volume1::chapter8::section1::subheader1_content::Content {}
+                },
                 (2, 3, 9, 1, 1) => rsx! {
                     section1::subheader1_content::Content {}
                 },
