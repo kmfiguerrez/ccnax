f06e3fb38d5789587815da8b450a4d3d802ad697
kmfiguerrez <kmfiguerrez@gmail.com>
2026-09-11 09:13:25 +0000

    Make v1p3c8s2sh1 content.

--- src/components/section_introduction.rs
+++ src/components/section_introduction.rs
@@ -20,6 +20,9 @@ pub fn SectionIntroduction(volume_id: u32, part_id: u32, chapter_id: u32, sectio
                 (1, 3, 8, 1) => rsx! {
                     volume1::chapter8::section1::SectionIntroductionContent {}
                 },
+                (1, 3, 8, 2) => rsx! {
+                    volume1::chapter8::section2::SectionIntroductionContent {}
+                },
                 _ => rsx! {
                     h3 { "Section Introduction not found!" }
                 },
--- src/components/subheader_content.rs
+++ src/components/subheader_content.rs
@@ -78,6 +78,10 @@ pub fn SubheaderContent(volume_id: u32, part_id: u32, chapter_id: u32, section_i
                 (1, 3, 8, 1, 2) => rsx! {
                     volume1::chapter8::section1::subheader2_content::Content {}
                 },
+                // Start of volume 1, part 3, chapter 8, section 2
+                (1, 3, 8, 2, 1) => rsx! {
+                    volume1::chapter8::section2::subheader1_content::Content {}
+                },
                 (2, 3, 9, 1, 1) => rsx! {
                     section1::subheader1_content::Content {}
                 },
