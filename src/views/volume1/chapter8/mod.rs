82b72785a20dccac9f2701b4ee690a3e29837270
kmfiguerrez <kmfiguerrez@gmail.com>
2026-09-11 02:20:14 +0000

    Make v1p3c8s1 content.

---
+++ src/views/volume1/chapter8/mod.rs
@@ -0,0 +1,4 @@
+mod chapter_introduction;
+pub use chapter_introduction::ChapterIntroductionContent;
+
+pub mod section1;
---
+++ src/views/volume1/chapter8/section1/mod.rs
@@ -0,0 +1,4 @@
+mod section_introduction;
+pub use section_introduction::SectionIntroductionContent;
+
+pub mod subheader1_content;
